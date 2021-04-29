0421103e6c6af651e32a40e096ee3b4b3f81422d
Simon Neb <simonneb@users.noreply.github.com>
2021-04-29 10:37:20 +0100

    Update config.js

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'FWS OLR footer page examples',
+  serviceName: 'FWS OLR footer page designs',
 
   // Default port that prototype runs on
   port: '3000',
